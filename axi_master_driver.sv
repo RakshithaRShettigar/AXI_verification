ae4612e0c8c658fc08a15d2c8a0579e7de1d0695
Akshay J Rai <107136628+axyrai@users.noreply.github.com>
2023-12-11 12:42:11 +0530

    Update axi_master_driver.sv

--- axi_master_driver.sv
+++ axi_master_driver.sv
@@ -1,11 +1,18 @@
+//AXI MASTER DRIVER IS THE USER DEFINED CLASS WHICH EXTENDS FROM UVM DRIVER (PREDEFINED DRIVER CLASS) AND THE MASTER TRANSACTION IS PASSED AS A PARAMETER
 
-
-class axi4_master_driver extends uvm_driver #(axi4_master_transaction); 
+class axi4_master_driver extends uvm_driver #(axi4_master_transaction);
+  // VIRTUAL INTERFACE HANDLE
   virtual axi_master_interface vif;
+  semaphore write_addr_data=new(1);
+  // TRANSACTION HANDLE
   axi4_master_transaction req;
 
+// FACTORY REGISTRATION
+// REGISTERING THE USER DEFINED CLASS IN THE LUT
 `uvm_component_utils (axi4_master_driver)
+  
 
+// DECLARING FUNCTIONS & TASKS EXTERNALLY
 extern function new(string name = "axi4_master_driver", uvm_component parent = null);
 
 extern virtual function void build_phase(uvm_phase phase);
@@ -17,12 +24,15 @@ extern virtual task axi4_write_task();
 extern virtual task axi4_read_task();
 
 endclass : axi4_master_driver 
+  
+// DEFINING THE CLASS CONSTRUCTOR OUTSIDE THE CLASS USING SCOPE RESOLUTION OPERATOR
 
 function axi4_master_driver::new(string name = "axi4_master_driver", uvm_component parent = null);
 	super.new(name, parent);
 
 endfunction: new
 
+// DEFINING BUILD PHASE OUTSIDE THE CLASS USING SCOPE RESOLUTION OPERATOR
 
 function void axi4_master_driver::build_phase(uvm_phase phase);
   	
@@ -33,10 +43,14 @@ function void axi4_master_driver::build_phase(uvm_phase phase);
 
 endfunction: build_phase
 
+// DEFINING RUN PHASE OUTSIDE THE CLASS USING SCOPE RESOLUTION OPERATOR
+
+// GETS THE SEQUENCER ITEMS FROM THE SEQUENCER AND THEN CONVERTS IT TO PIN LEVEL AND SENDS IT TO THE DUT
+
 task axi4_master_driver::run_phase(uvm_phase phase);
   forever
     begin
-      if(!vif.rst)
+      if(!vif.rst) // CHECKING FOR RESET CONDITION
         begin
           @(posedge vif.axi_master_dr_mp.clk)
           vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_arvalid <=0;
@@ -47,18 +61,22 @@ task axi4_master_driver::run_phase(uvm_phase phase);
         end
       else
         begin
-          seq_item_port.get_next_item(req);
+          seq_item_port.get_next_item(req); //SEQUENCE-DRIVER HANDSHAKING MECHANISM
+          
+//THE WRITE AND READ TASK DECLARED INSIDE THE FORK-JOIN (without begin end) STATEMENT FOR PARALLEL PROCESSING
           fork 
-    	    axi4_write_task();
+            axi4_write_task();
     	    axi4_read_task();
           join
         end
-      seq_item_port.item_done();
+      seq_item_port.item_done(); //SEQUENCE-DRIVER HANDSHAKING MECHANISM
     end
 endtask
 
+  //MASTER DRIVER WRITE TASK
 task axi4_master_driver::axi4_write_task();
   fork
+    //WRITE ADDRESS CHANNEL LOGIC
 	begin: WRITE ADDRESS CHANNEL
       if(req.s_axi_awvalid)
         begin
@@ -76,7 +94,7 @@ task axi4_master_driver::axi4_write_task();
                   
                   @(posedge vif.axi_master_dr_mp.clk)
                   begin
-                    
+                    write_addr_data.get(1);
                     if(req.s_axi_awburst==2'b00) //fixed type
                       begin
                         vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_awaddr <= req.s_axi_awaddr;
@@ -88,10 +106,11 @@ task axi4_master_driver::axi4_write_task();
                         vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_awcache <= req.s_axi_awcache;
                         vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_awprot <= req.s_axi_awprot;
                         vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_awvalid <= req.s_axi_awvalid;
+                        
                       end
                     else if(req.s_axi_burst==2'b01)  //Incr type
                       begin
-                        vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_awaddr <= req.s_axi_awaddr+((len+1)*totalBytes);
+                        vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_awaddr <= req.s_axi_awaddr+((i)*totalBytes);
                         vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_awlen <= req.s_axi_awlen;
                         vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_awid <= req.s_axi_awid;
                         vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_awsize <= req.s_axi_awsize;
@@ -100,6 +119,7 @@ task axi4_master_driver::axi4_write_task();
                         vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_awcache <= req.s_axi_awcache;
                         vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_awprot <= req.s_axi_awprot;
                         vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_awvalid <= req.s_axi_awvalid;
+                        
                       end
                     
                   end
@@ -108,7 +128,7 @@ task axi4_master_driver::axi4_write_task();
         end
 
 	end: WRITE ADDRESS CHANNEL
-
+// WRITE DATA LOGIC
 	begin: WRITE DATA 
       
      
@@ -117,71 +137,76 @@ task axi4_master_driver::axi4_write_task();
           @(posedge vif.axi_master_dr_mp.clk)
           begin
             if(vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_wready)
-              begin
-                foreach(s_axi_wstrb)
                   begin
-                    if(s_axi_wstrb[i]==0)
-                      begin
-                        vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_wdata[8*(i+1)-1 : 8*(i+1)-8] <= req.s_axi_wdata[8*(i+1)-1 : 8*(i+1)-8];
-                        vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_wvalid <= req.s_axi_wvalid;
-                        vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_wlast <= req.s_axi_wlast;
-                      end
+                    vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_wdata <= req.s_axi_wdata.pop_front();
+                    vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_wstrb <= req.s_axi_wstrb.pop_front();
+                    vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_wvalid <= 1'b1;
+                    if(req.s_axi_wdata.size()>0)
+                       vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_wlast<= 0;
+                    else
+                       vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_wlast<= 1'b1;
+                    write_addr_data.put(1);
                   end
-              end
           end
-          
         end
 
-    end: WRITE DATA CHANNEL
-
-	begin: WRITE RESPONSE CHANNEL
+    end: WRITE DATA 
+      // WRITE RESPONSE CHANNEL LOGIC
+    begin: WRITE RESPONSE CHANNEL
+      if(vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_bvalid)
+        begin
+          @(posedge vif.axi_master_dr_mp.clk)
+          begin
+            if(req.s_axi_bready)
+              begin
+               req.s_axi_bvalid <= 1'b1;
+              end
+          end
 	
 	end: WRITE RESPONSE CHANNEL
 join
 
 endtask: axi4_write_task
 
+ // MASTER DRIVER READ TASK
 
 task axi4_master_driver::axi4_read_task();
-
-axi_read_seq_item_port.get_next_item(req_rd);
 begin
 fork
+  // READ ADDRESS CHANNEL LOGIC
 	begin: READ ADDRESS CHANNEL
       if(req.s_axi_arvalid)
         begin
-          if(vif.s_axi_arready)
-            begin
-              vif.s_axi_arid <= req.s_axi_arid;
-              vif.s_axi_araddr <= req.s_axi_araddr;
-              vif.s_axi_arlen <= req.s_axi_arlen;
-              vif.s_axi_arsize <= req.s_axi_arsize;
-              vif.s_axi_arburst <= req.s_axi_arburst;
-              vif.s_axi_arlock <= req.s_axi_arlock;
-              vif.s_axi_arcache <= req.s_axi_arcache;
-              vif.s_axi_arprot <= req.s_axi_arprot;
-              vif.s_axi_arvalid <= 1'b1;
+          @(posedge vif.axi_master_dr_mp.clk)
+          begin
+            if(vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_arready)
+              begin
+              vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_arid <= req.s_axi_arid;
+              vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_araddr <= req.s_axi_araddr;
+              vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_arlen <= req.s_axi_arlen;
+              vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_arsize <= req.s_axi_arsize;
+              vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_arburst <= req.s_axi_arburst;
+              vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_arlock <= req.s_axi_arlock;
+              vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_arcache <= req.s_axi_arcache;
+              vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_arprot <= req.s_axi_arprot;
+              vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_arvalid <= 1'b1;
             end
         end
     end: READ ADDRESS CHANNEL
+ 
+ // READ DATA CHANNEL LOGIC     
 
 	begin: READ DATA CHANNEL
-      if(req.s_axi_rvalid)
+      if(vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_rvalid)
         begin
-          if(vif.s_axi_rready)
+          if(req.s_axi_rready)
             begin
-              vif.s_axi_rid <= req.s_axi_rid;
-              vif.s_axi_rdata <= req.s_axi_rdata;
-              vif.s_axi_rresp <= req.s_axi_rresp;
-              vif.s_axi_rlast <= req.s_axi_rlast;
-              vif.s_axi_rvalid <= 1'b1;
+              vif.axi_master_dr_mp.axi_master_dr_cb.s_axi_rready <= 1'b1;
             end
         end
     end: READ DATA CHANNEL
 
 join
-  
-axi4_read_seq_item_port.item_done();
 
 endtask : axi4_read_task
 
