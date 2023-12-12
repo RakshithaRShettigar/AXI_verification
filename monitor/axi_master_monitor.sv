79923d0a3c29705175804b2a5fd67d104a5fcf17
Rakshitha R <109401667+RakshithaRShettigar@users.noreply.github.com>
2023-12-12 10:26:50 +0530

    Update axi_master_monitor.sv

--- monitor/axi_master_monitor.sv
+++ monitor/axi_master_monitor.sv
@@ -1,7 +1,7 @@
 //AXI_MASTER_MONITOR PSEUDOCODE:
 //-----------------------------------------------------------------------------------
 //axi_master_monitor is user-defined class which is extended from uvm_monitor which is a pre-defined uvm class
-class axi_master_monitor extends uvm_monitor 
+class axi_master_monitor extends uvm_monitor;
 
 //Factory registration
 `uvm_component_utils(axi_master_monitor)
@@ -57,14 +57,14 @@ task axi_master_monitor::run_phase(uvm_phase phase);
                 @(posedge vif.m_mp.clk);
               end
               while(vif_m_mp.m_cb.awvalid != 1 && vif.m_mp.m_cb.awready != 1);
-              req_op.awid    = vif.m_mp.m_cb.awid ;
-              req_op.awaddr  = vif.m_mp.m_cb.awaddr;
-              req_op.awlen   = vif.m_mp.m_cb.awlen;
-              req_op.awsize  = vif.m_mp.m_cb.awsize;
-              req_op.awburst =vif.m_mp.m_cb.vawburst;
-              req_op.awlock  = vif.m_mp.m_cb.awlock;
-              req_op.awcache = vif.m_mp.m_cb.awcache;
-              req_op.awprot  = vif.m_mp.m_cb.awprot;
+              req_op.s_axi_awid    = vif.m_mp.m_cb.s_axi_awid ;
+              req_op.s_axi_awaddr  = vif.m_mp.m_cb.s_axi_awaddr;
+              req_op.s_axi_awlen   = vif.m_mp.m_cb.s_axi_awlen;
+              req_op.s_axi_awsize  = vif.m_mp.m_cb.s_axi_awsize;
+              req_op.s_axi_awburst =vif.m_mp.m_cb.s_axi_awburst;
+              req_op.s_axi_awlock  = vif.m_mp.m_cb.s_axi_awlock;
+              req_op.s_axi_awcache = vif.m_mp.m_cb.s_axi_awcache;
+              req_op.s_axi_awprot  = vif.m_mp.m_cb.s_axi_awprot;
             end : WRITE_ADDRESS
             
             begin : WRITE_DATA
@@ -74,11 +74,13 @@ task axi_master_monitor::run_phase(uvm_phase phase);
               do begin
                 @(posedge vif.m_mp.clk);
               end
-                while(vif.m_mp.m_cb.wvalid != 1 && vif.m_mp.m_cb.wready != 1);
-               req_op.wdata[i] = vif.m_mp.m_cb.wdata;
-               req_op.wstrb[i] = vif.m_mp.m_cb.wstrb;
-               req_op.wuser[i] = vif.m_mp.m_cb.wuser;
-               req_op.wlast = vif.m_mp.m_cb.wlast;
+                while(vif.m_mp.m_cb.s_axi_wvalid != 1 && vif.m_mp.m_cb.s_axi_wready != 1);
+               req_op.s_axi_wdata[i] = vif.m_mp.m_cb.s_axi_wdata;
+               req_op.s_axi_wstrb[i] = vif.m_mp.m_cb.s_axi_wstrb;
+               req_op.s_axi_wuser[i] = vif.m_mp.m_cb.s_axi_wuser;
+               req_op.s_axi_wlast = vif.m_mp.m_cb.s_axi_wlast;
+               req_op.s_axi_wvalid  = vif.m_mp.m_cb.s_axi_wvalid;
+               req_op.s_axi_wready  = vif.m_mp.m_cb.s_axi_wready;
                 if(req_op.wlast == 1) begin
                   i = 0;
                   break;
@@ -91,9 +93,11 @@ task axi_master_monitor::run_phase(uvm_phase phase);
           do begin
             @(posedge vif.m_mp.clk);
           end
-          while(vif.m_mp.m_cb.bvalid != 1 && bready != 1);
-         req_op.bid      = bid;
-         req_op.bresp    = bresp;
+          while(vif.m_mp.m_cb.s_axi_bvalid != 1 && vif.m_mp.m_cb.s_axi_bready != 1);
+         req_op.s_axi_bid      = vif.m_mp.m_cb.s_axi_bid;
+         req_op.s_axi_bresp    = vif.m_mp.m_cb.s_axi_bresp;
+         req_op.s_axi_bvalid   = vif.m_mp.m_cb.s_axi_bvalid;
+         req_op.s_axi_bready   = vif.m_mp.m_cb.s_axi_bready;
           end : WRITE_RESPONSE   
       end : WRITE_PROCESS
       
@@ -102,27 +106,31 @@ task axi_master_monitor::run_phase(uvm_phase phase);
               do begin
                 @(posedge vif.m_mp.clk);
               end
-                while(awvalid != 1 && awready != 1);
-              req_op.rwid    = vif.m_mp.m_cb.rwid ;
-              req_op.rwaddr  = vif.m_mp.m_cb.rwaddr;
-              req_op.rwlen   = vif.m_mp.m_cb.rwlen;
-              req_op.rwsize  = vif.m_mp.m_cb.rwsize;
-              req_op.rwburst = vif.m_mp.m_cb.rwburst;
-              req_op.rwlock  = vif.m_mp.m_cb.rwlock;
-              req_op.rwcache = vif.m_mp.m_cb.rwcache;
-              req_op.rwprot  = vif.m_mp.m_cb.rwprot;
-        
+              while(vif.m_mp.m_cb.s_axi_arvalid != 1 && vif.m_mp.m_cb.s_axi_arready != 1);
+              req_op.s_axi_arid    = vif.m_mp.m_cb.s_axi_arid ;
+              req_op.s_axi_araddr  = vif.m_mp.m_cb.s_axi_araddr;
+              req_op.s_axi_arlen   = vif.m_mp.m_cb.s_axi_arlen;
+              req_op.s_axi_arsize  = vif.m_mp.m_cb.s_axi_arsize;
+              req_op.s_axi_arburst = vif.m_mp.m_cb.s_axi_arburst;
+              req_op.s_axi_arlock  = vif.m_mp.m_cb.s_axi_arlock;
+              req_op.s_axi_arcache = vif.m_mp.m_cb.s_axi_arcache;
+              req_op.s_axi_arprot  = vif.m_mp.m_cb.s_axi_arprot;
+              req_op.s_axi_arvalid = vif.m_mp.m_cb.s_axi_arvalid;
+              req_op.s_axi_arready = vif.m_mp.m_cb.s_axi_arready;
+          
               static int j;
               //Taking data of read data channel
               do begin
                 @(posedge vif.m_mp.clk);
               end
-              while(rvalid != 1 && rready != 1);
+              while(vif.m_mp.m_cb.s_axi_rvalid != 1 && vif.m_mp.m_cb.s_axi_rready != 1);
                req_op.rid = vif.m_mp.m_cb.rid;
-                req_op.rdata[j] = vif.m_mp.m_cb.rdata;
-               req_op.ruser = vif.m_mp.m_cb.ruser;
-               req_op.rresp = vif.m_mp.m_cb.rresp;
-               req_op.rlast = vif.m_mp.m_cb.rlast;
+               req_op.rdata[j] = vif.m_mp.m_cb.s_axi_rdata;
+               req_op.s_axi_ruser = vif.m_mp.m_cb.s_axi_ruser;
+               req_op.s_axi_rresp = vif.m_mp.m_cb.s_axi_rresp;
+               req_op.s_axi_rlast = vif.m_mp.m_cb.s_axi_rlast;
+               req_op.s_axi_rvalid = vif.m_mp.m_cb.s_axi_rvalid;
+               req_op.s_axi_rvalid = vif.m_mp.m_cb.s_axi_rvalid;
                 if(req_op.rlast == 1) begin
                   j = 0;
                   break;
@@ -137,3 +145,4 @@ task axi_master_monitor::run_phase(uvm_phase phase);
     wait fork;
   end
 endtask
+      
