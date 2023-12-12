ab38333cdf1c99736943da26a818ca52256d7680
Akshay J Rai <107136628+axyrai@users.noreply.github.com>
2023-12-12 10:47:11 +0530

    Create axi_master_sequencer.sv

---
+++ axi_master_sequencer.sv
@@ -0,0 +1,9 @@
+class axi_master_sequencer extends uvm_sequencer #(axi_master_transaction);
+   `uvm_component_utils(axi_master_sequencer)
+  
+extern function new(string name = "axi_master_sequencer", uvm_component parent);
+endclass
+function axi4_master_sequencer::new(string name = "axi_master_sequencer",
+                                uvm_component parent = null);
+   super.new(name, parent);
+endfunction
